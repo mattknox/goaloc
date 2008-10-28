dd781df96e9af35f4b16a9c351a18c8b0cf54767
matt knox <matthewknox@gmail.com>
2008-10-28 19:44:41 -0400

    skeleton'd out the merb generator

--- lib/goaloc/generators/merb.rb
+++ lib/goaloc/generators/merb.rb
@@ -1,5 +1,20 @@
 require "erb"
 
 class Merb < Generator
-  
+  def app_name(opts = { })
+    name = app.name
+    name << "_merb" if opts[:prefix]
+    name
+  end
+
+  def gen_routes
+  end
+  def gen_migration(model)
+  end
+  def gen_model(model)
+  end
+  def gen_controller(model)
+  end
+  def gen_view(model)
+  end
 end
