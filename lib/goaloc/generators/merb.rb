89f9dfdeede5cfcf2842afa1d932269a0527ab1a
matt knox <matthewknox@gmail.com>
2008-11-21 18:56:20 -0500

    added comment to server and started fleshing out the merb generator

--- lib/goaloc/generators/merb.rb
+++ lib/goaloc/generators/merb.rb
@@ -1,12 +1,29 @@
 require "erb"
 
 class Merb < Generator
+  cattr_accessor :merb_models
+  self.merb_models = []
+
   def app_name(opts = { })
     name = app.name
     name << "_merb" if opts[:prefix]
     name
   end
 
+  def generate
+    app.models.values.each do |m|
+      merb_models  << merbify(m)
+    end
+    
+    gen_app
+    merb_models.each do |merb_model|
+      gen_routes
+      gen_migration(merb_model)
+      gen_model(merb_model)
+      gen_controller(merb_model)
+      gen_view(merb_model)
+    end
+  end
   # gonna get Foy to help with this.
   def gen_routes
   end
--- lib/goaloc/server/server.rb
+++ lib/goaloc/server/server.rb
@@ -1,3 +1,4 @@
 class Server
   # this is intended to serve a goaloc app directly, before it has been generated.
+  # this should use rack to actually serve the files, probably via mongrel or something.
 end
