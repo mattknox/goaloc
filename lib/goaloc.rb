81b2e85475c590972e5e117734d3f9969e05c971
matt knox <matthewknox@gmail.com>
2009-01-12 18:30:44 -0500

    starting the reimplementation

--- lib/goaloc.rb
+++ lib/goaloc.rb
@@ -3,28 +3,7 @@ $:.unshift(File.dirname(__FILE__)) unless
 
 require "rubygems"
 require "activesupport"
-require "goaloc/app"
-require "goaloc/model"
-require "goaloc/generators/generator"
-require "goaloc/generators/rails"
-require "goaloc/generators/merb"
+require "goaloc/goal"
 
 module Goaloc
-end
-
-APP = @app = App.new
-
-def generate(*args)
-  @app.log << "generate #{args.inspect[1..-2]}"
-  @app.generate(*args)
-end
-
-def route(*args)
-  @app.log << "route #{args.inspect[1..-2]}"
-  @app.route(*args)
-end
-
-def add_attrs(h)
-  @app.log << "add_attrs #{h.inspect[1..-2]}"
-  @app.add_attrs(h)
 end
---
+++ lib/goaloc/goal.rb
@@ -0,0 +1,48 @@
+class Goal
+  attr_reader :name
+  attr_accessor :associations, :validations, :fields, :options, :routes
+
+  def initialize(name)
+    @name = name.underscore.singularize   # TODO: support renaming models
+    self.associations = { }
+    self.validations = []
+    self.fields = { }
+    self.options = { }
+    self.routes = [] # of the form [:classname, [:otherclass, :classname], ...]
+  end
+
+  # here are a list of name-ish methods
+  def foreign_key
+    self.name + "_id"
+  end
+
+  def s self.name;  end
+  def p self.name.pluralize;    end
+  def s self.name.camelize.singularize;    end
+  def s self.name.camelize.pluralize;      end
+
+  # association stuff
+  def belongs_to(goal)
+    self.associations[goal.name] = { :goal => goal, :type => :belongs_to }
+    self.fields[goal.foreign_key] = "reference"
+  end
+
+  def has_many(goal)
+    self.associations[goal.name.pluralize] = { :goal => goal, :type => :has_many }
+    goal.belongs_to(self)
+  end
+
+  def has_one(goal)
+    self.associations[goal.name] = { :goal => goal, :type => :has_one }
+    goal.belongs_to(self)
+  end
+
+  def associate(assoc_type, goal, options = { })
+    assoc_name = options[:assoc_name] || goal.default_assoc_name(assoc_type)
+    self.associations[assoc_name] = { :goal => goal, :type => assoc_type }
+  end
+
+  def default_assoc_name(assoc_type)
+    :has_many == assoc_type ? name.pluralize : name
+  end
+end
