54ae22f1d51ba268c2622512344d93b57e860b8a
matt knox <matthewknox@gmail.com>
2009-01-16 02:29:35 -0500

    started adding the test for generator.rb

---
+++ test/test_generator.rb
@@ -0,0 +1,9 @@
+require File.dirname(__FILE__) + '/test_helper'
+
+class TestGenerator < Test::Unit::TestCase
+  context "the base generator" do
+    should "have an app"
+    should "have a generate method"
+    should "have a generate_all method"
+  end
+end
