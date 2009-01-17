78a8911b30e38dba1041431dab2997f80e33d15b
matt knox <matthewknox@gmail.com>
2009-01-16 19:24:21 -0500

    started filling in tests.

--- test/test_rails_generator.rb
+++ test/test_rails_generator.rb
@@ -1,5 +1,20 @@
 require File.dirname(__FILE__) + '/test_helper'
 
 class TestRailsGenerator < Test::Unit::TestCase
-  
+  context "a rails generator with one empty goal" do
+    should "produce a valid string for generate routes"
+  end
+
+  context "a rails generator with a nontrivial goal" do
+    should "produce a valid string for the model"
+    should "produce a valid string for the controller"
+    should "produce a valid string for the index view"
+    should "produce a valid string for the show view"
+    should "produce a valid string for the _model view"
+    should "produce a valid string for the _model_small view"
+    should "produce a valid string for the _form view"
+    should "produce a valid string for the edit view"
+    should "produce a valid string for the new view"
+    should "produce a valid string for the view layout"
+  end
 end
