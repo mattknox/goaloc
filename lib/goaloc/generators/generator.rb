7c3dd3dbaaad85d46083f04e25004fb9cab32985
matt knox <matthewknox@gmail.com>
2009-01-17 12:30:53 -0500

    fixed up the generator, removing dead code and commenting a method.

--- lib/goaloc/generators/generator.rb
+++ lib/goaloc/generators/generator.rb
@@ -1,16 +1,9 @@
 class Generator
-  attr_accessor :app, :opts
-
-  def initialize(app, target, opts = { })
-    @app = app
-    @opts = opts
-  end
-
-  def Generator.generate_all(opts = { })
-    self.subclasses.each do |str|
-      str.constantize.new(app, opts.merge(:prefix => true)).generate
-    end
-  end
+#   def Generator.generate_all(opts = { })
+#     self.subclasses.each do |str|
+#       str.constantize.new(app, opts.merge(:prefix => true)).generate
+#     end
+#   end
 
   def Generator.build(app, target, opts = { })
     if Generator.subclasses.member?(target.to_s)
@@ -19,6 +12,4 @@ class Generator
       raise RuntimeError
     end
   end
-  # TODO: move a lot of the duplicated stuff from rails/merb into here.
-  #   then the various generators just have to provide appropriate templates.  
 end
--- test/test_generator.rb
+++ test/test_generator.rb
@@ -13,15 +13,11 @@ class TestGenerator < Test::Unit::TestCase
       end
     end
     
-    should "have an app" do
-      assert @generator.app
-    end
-    
     should "have a generate method" do
       assert @generator.respond_to?(:generate)
     end
     
-    should "have a generate_all method" do
+    should_eventually "have a generate_all method" do
       assert Generator.respond_to?(:generate_all)
     end
   end
