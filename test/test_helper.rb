9005ee99eb63da12af20d84265cca884c463bbce
matt knox <matthewknox@gmail.com>
2009-01-17 18:13:50 -0500

    added test_helpers stolen from technicalpickles's jeweler

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,19 @@ require File.dirname(__FILE__) + '/../lib/goaloc'
 require "Shoulda"
 require 'rubygems'
 require 'mocha'
+
+def self.should_create_directory(directory)
+  should "create #{directory} directory" do
+    assert File.exists?(File.join(@tmp_dir, directory))
+    assert File.directory?(File.join(@tmp_dir, directory))
+  end
+end
+
+def self.should_create_files(*files)
+  should "create #{files.join ', '}" do
+    files.each do |file|
+      assert File.exists?(File.join(@tmp_dir, file))
+      assert File.file?(File.join(@tmp_dir, file))        
+    end
+  end
+end
