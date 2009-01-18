eb7db6a8b46c1680ee57c5b2db07f9282efbe390
matt knox <matthewknox@gmail.com>
2009-01-17 22:01:41 -0500

    syntax fix

--- lib/goaloc/generators/rails.rb
+++ lib/goaloc/generators/rails.rb
@@ -98,10 +98,10 @@ class Rails < RubyGenerator
 #       gen_view(goal)
 #       gen_tests(goal)
 #       gen_misc
-#     end
+    end
     self
   end
-
+  
   def gen_app
     unless File.exists?(app_dir) #TODO: figure out what to do when there is a directory there.
       original_dir = FileUtils.pwd
