2fb71a7f34400ed6fa02a94ff8e13846cdc1424c
matt knox <matthewknox@gmail.com>
2009-01-30 18:52:59 -0500

    added spots to put the activerecord/actionview stuff from rails.rb

---
+++ lib/goaloc/generators/action_view_gen.rb
@@ -0,0 +1,3 @@
+class ActionViewGen
+  # move all of the ActionView stuff into here from rails.rb
+end
--- lib/goaloc/generators/ar_model.rb
+++ lib/goaloc/generators/ar_model.rb
@@ -1,2 +1,3 @@
 class ArModel
+  # TODO: move all the activerecord-specific stuff from rails into here.
 end
