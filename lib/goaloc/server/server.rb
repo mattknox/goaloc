c07d8fdd349d74443cd08d48dd645414ed189280
matt knox <matthewknox@gmail.com>
2009-02-11 03:34:52 -0500

    janky commit.

--- lib/goaloc/server/server.rb
+++ lib/goaloc/server/server.rb
@@ -1,4 +1,4 @@
 class Server
-  # this is intended to serve a goaloc app directly, before it has been generated.
+  # this is intended to serve a goaloc app directly, before it has been generated into code for another, lesser framework.
   # this should use rack to actually serve the files, probably via mongrel or something.
 end
