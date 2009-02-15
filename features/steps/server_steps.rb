f90f1a5a3942b10419f10121e5f9f1ff562cff54
matt knox <matthewknox@gmail.com>
2009-02-15 09:51:25 -0500

    commented empty steps to make my rake cucumber output accurate.

--- features/steps/server_steps.rb
+++ features/steps/server_steps.rb
@@ -1,11 +1,11 @@
 require "rubygems"
 require "spec"
 
-Given /^an? (.*) app$/ do |desc|
-end
+# Given /^an? (.*) app$/ do |desc|
+# end
 
-When /^I GET the "([^"]+). page$/ do |page|
-end
+# When /^I GET the "([^"]+). page$/ do |page|
+# end
 
-Then /^I should see an html page$/ do
-end
+# Then /^I should see an html page$/ do
+# end
