ae3a6c53385105f9fbcde3a0f69153c09b99ff72
matt knox <matthewknox@gmail.com>
2009-02-23 18:55:12 -0500

    added the beginnings of a rack handler.

--- lib/goaloc/server/server.rb
+++ lib/goaloc/server/server.rb
@@ -2,3 +2,7 @@ class Server
   # this is intended to serve a goaloc app directly, before it has been generated into code for another, lesser framework.
   # this should use rack to actually serve the files, probably via mongrel or something.
 end
+app = proc do |env|
+  [200, { 'Content-Type' => 'text/html' }, 'no pepper plz!']
+end
+run app
