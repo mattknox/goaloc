217c0e73910d7eca73027587986fac7d7517122c
matt knox <matthewknox@gmail.com>
2009-02-25 18:14:36 -0500

    covered the reset method.

--- test/test_goaloc.rb
+++ test/test_goaloc.rb
@@ -2,7 +2,11 @@ require File.dirname(__FILE__) + '/test_helper'
 
 class TestApp < Test::Unit::TestCase
   context "the core goaloc class" do
-
-    should "cover the test methods"
+    
+    should "have a working reset method" do
+      old_app = APP
+      Object.send(:reset)
+      assert_not_equal old_app, @app
+    end
   end
 end
