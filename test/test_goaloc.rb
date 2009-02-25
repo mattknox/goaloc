ef6401428f7b4f20572b9d0cb8183b59547ed030
matt knox <matthewknox@gmail.com>
2009-02-25 18:18:54 -0500

    renamed test class.

--- test/test_goaloc.rb
+++ test/test_goaloc.rb
@@ -1,6 +1,6 @@
 require File.dirname(__FILE__) + '/test_helper'
 
-class TestApp < Test::Unit::TestCase
+class TestGoaloc < Test::Unit::TestCase
   context "the core goaloc class" do
     
     should "have a working reset method" do
@@ -8,5 +8,10 @@ class TestApp < Test::Unit::TestCase
       Object.send(:reset)
       assert_not_equal old_app, @app
     end
+  end
+
+  should "route things " do
+#    assert !!route
+#    assert_match /post/, Object.send(:showlog)
   end
 end
