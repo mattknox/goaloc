05565e77645f190773d780609cf18b78fbb40aa3
matt knox <matthewknox@gmail.com>
2009-03-04 21:24:42 -0500

    started adding rspec.  will start using shoulda in the rspec next.

---
+++ spec/goaloc.rb
@@ -0,0 +1,11 @@
+require File.dirname(__FILE__) + '/../lib/goaloc'
+
+describe Goaloc do
+  before(:each) do
+    @app = App.new
+  end
+
+  it "should do stuff" do
+    true.should == true
+  end
+end
