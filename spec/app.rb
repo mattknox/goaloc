309cd164c13c6034a8490d964c9543abddd06e3c
matt knox <matthewknox@gmail.com>
2009-03-06 18:46:05 -0500

    started spec-ing stuff in RSpec.

--- spec/app.rb
+++ spec/app.rb
@@ -1,11 +1,11 @@
 require File.dirname(__FILE__) + '/../lib/goaloc'
 
-describe Goaloc do
+describe App do
   before(:each) do
     @app = App.new
   end
 
   it "should do stuff" do
-    @app.should respond_to :route
+    @app.should respond_to :route    
   end
 end
