b890df8e6f01f71f2f71a34cb60a42da35ec5068
matt knox <matthewknox@gmail.com>
2009-03-06 19:00:11 -0500

    dipping my toe into rspec.

--- spec/app.rb
+++ spec/app.rb
@@ -8,5 +8,12 @@ describe App do
   it "should route things" do
     @app.should respond_to :route
     @app.goals.length.should == 0
+    @app.route [:posts, :comments]
+    @app.goals.length.should == 2
+    @app.routes.length.should == 1
+  end
+
+  it "should allow adding attrs to models" do
+    @app.should respond_to :add_attrs
   end
 end
