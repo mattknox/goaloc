897b1219ed7237a49eb08260c8b08bfcae8c4af4
matt knox <matthewknox@gmail.com>
2009-03-10 18:20:30 -0400

    more changes to the server.

--- lib/goaloc/server/server.rb
+++ lib/goaloc/server/server.rb
@@ -1,8 +1,9 @@
 class Server
   # this is intended to serve a goaloc app directly, before it has been generated into code for another, lesser framework.
   # this should use rack to actually serve the files, probably via mongrel or something.
+
+  @app = proc do |env|
+    [200, { 'Content-Type' => 'text/html' }, "no content yet"]
+  end
+  Rack::Handler::WEBrick.run app, :Port => 2000
 end
-app = proc do |env|
-  [200, { 'Content-Type' => 'text/html' }, "no content yet"]
-end
-run app
