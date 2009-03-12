47f83f721ec3a280c22ce3818c6e071dfab6d87d
matt knox <matthewknox@gmail.com>
2009-03-12 13:47:46 -0400

    further changes to server.

--- lib/goaloc/server/server.rb
+++ lib/goaloc/server/server.rb
@@ -1,11 +1,17 @@
+require "rack"
 class Server
   # this is intended to serve a goaloc app directly, before it has been generated into code for another, lesser framework.
   # this should use rack to actually serve the files, probably via mongrel or something.
 
   def call(env)
-    [200, { 'Content-Type' => 'text/html' }, "no content yet"]
+    puts env.inspect
+    respond(env["REQUEST_METHOD"] + ": " + env["PATH_INFO"])
   end
 
+  def respond(content, status_code = 200, headers = { 'Content-Type' => 'text/html' })
+    [status_code, headers, content]
+  end
+  
   def serve
     Rack::Handler::WEBrick.run self, :Port => 9292
   end
