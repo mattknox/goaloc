a75187277f87fa1134b5c043b2977d1fdc20d593
matt knox <matthewknox@gmail.com>
2010-07-07 21:23:33 -0700

    further changes

---
+++ lib/goaloc/extensions.rb
@@ -0,0 +1,35 @@
+# the intent of this class is to let people write a goaloc extension,
+# which will take in a configuration and return a preconfigured chunk
+# of app.  For example:
+# route [:site, Blog.new(:comments => :moderated), Wiki.new(:extensions => [:syntax_highlight, :upvotes])]
+# might put a blog with moderated contents and a wiki with some
+# extensions under /sites for a given app.
+
+class GoalocExtension
+  def initialize(config = { }, &block)
+    @config = config
+    yield(self) if block_given?
+    self
+  end
+
+  def run
+    raise "this method should be overridden by subclasses"
+  end
+end
+
+class Blog < GoalocExtension
+  # fill in here the logic for building a blog up.  Should at minimum
+  # attach comments, possibly handle auth, etc..
+
+  def initialize(author = :User, commentor = :User)
+    @author = author
+    @commentor = commentor
+  end
+
+  def run(app, route_prefix)
+    app.route_elt([:post, :comment], route_prefix)
+    app.add_attrs :post => "title:string body:text", :comment => "body:text"
+    Post.belongs_to @author
+    Comment.belongs_to @commentor
+  end
+end
--- spec/app.rb
+++ spec/app.rb
@@ -5,7 +5,7 @@ describe App do
     @app = App.new
   end
 
-  it "should route things" do
+  it "should route symbols" do
     @app.should respond_to :route
     @app.goals.length.should == 0
     @app.route [:posts, :comments]
@@ -19,5 +19,9 @@ describe App do
     @app.goals[:post].fields.length.should == 0
     @app.add_attrs :posts => "title:string body:text"
     @app.goals[:post].should satisfy { |arg| arg.fields[:body] == "text" }
+  end
+
+
+  it "should route extensions" do
   end
 end
